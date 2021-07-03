5aa87f108bbc682d3e0bc66c37a749b320415e84
mamanshsoni <mamanshsoni@gmail.com>
2021-07-03 08:14:47 +0530

    initialized new project and installed dev dep

---
+++ nodejs/script.js
@@ -0,0 +1,11 @@
+let str="Asdasd"
+let num =233
+let  bool=true
+let arr = [24,46,45,65]
+let obj = {a:10, b:'asgd'}
+
+//if x and y are not premitive
+// x==y : true <- what does this mean ?
+// this means they are reference to the same objects in memory.
+
+console.log(typeof String);
