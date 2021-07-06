b8a4012e9812e7f6eaf4d2813e45de30d128a112
mamanshsoni <mamanshsoni@gmail.com>
2021-07-07 00:06:41 +0530

    displayed show results and small fetctcht fetch refractor

--- box-office/src/pages/Home.js
+++ box-office/src/pages/Home.js
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
   const onSearch = () => {
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+      console.log(result);
+    });
     // https://api.tvmaze.com/search/shows?q=mens
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-        console.log(result);
-      });
+    // fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+    //   .then(r => r.json())
+    //   .then(result => {
+    //     setResults(result);
+    //     console.log(result);
+    //   });
   };
 
   const onInputChange = ev => {
@@ -25,6 +32,22 @@ const Home = () => {
     }
   };
 
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div> No results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
     <MainPageLayout>
       <input
@@ -36,6 +59,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
