680cd30d3a996c762162a1e080a23cdcbca98f4c
mamanshsoni <mamanshsoni@gmail.com>
2021-07-17 14:25:03 +0530

    finnished starred page

--- box-office/src/components/show/ShowMainData.js
+++ box-office/src/components/show/ShowMainData.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import IMG_PLACEHOLDER from '../../images/not-found.png';
 import { Star } from '../styled';
-import { Headline, MainDataWrapper, TagList } from './ShowMainData.styled';
+import { MainDataWrapper, Headline, TagList } from './ShowMainData.styled';
 
 const ShowMainData = ({ name, rating, summary, tags, image }) => {
   return (
@@ -12,7 +12,7 @@ const ShowMainData = ({ name, rating, summary, tags, image }) => {
         <Headline>
           <h1>{name}</h1>
           <div>
-            <Star />
+            <Star active />
             <span>{rating.average || 'N/A'}</span>
           </div>
         </Headline>
@@ -21,14 +21,14 @@ const ShowMainData = ({ name, rating, summary, tags, image }) => {
           dangerouslySetInnerHTML={{ __html: summary }}
         />
 
-        <TagList>
+        <div>
           Tags:{' '}
-          <div>
+          <TagList>
             {tags.map((tag, i) => (
               <span key={i}>{tag}</span>
             ))}
-          </div>
-        </TagList>
+          </TagList>
+        </div>
       </div>
     </MainDataWrapper>
   );
